4c7c2051dce5444dfd6cc1f3f4437165f9c5104f
73613398 <a.qureshi0327@gmail.com>
2023-01-19 18:10:15 -0500

    going to deleted

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,8 @@ import '../../App.css';
 import React from 'react';
 import HeroSection from '../HeroSection.js';
 import Header from '../Header.js';
-import {Container} from "@mui/material";
+import {Button, Container} from "@mui/material";
+import { Link } from "react-router-dom";
 import { Box } from '@mui/system';
 
 
@@ -12,85 +13,49 @@ function Home() {
     <>
 
     {/* Header */}
-      <Container>
+      <Box>
         <Header />
-      </Container>
+      </Box>
     {/* About Us Title*/}
-      <Container sx={{marginTop: '10%', marginRight: '10px'}}>
-        <Box className='title'
-          sx={{
-            position:'relative',
-            display: 'flex',
-            justifyContent: 'flex-end',
-          
-          }}
-        >
-          <Box className='square' 
-            sx={{
-              width:'auto',
-              height: 'auto',
-              position: 'absolute',
-              textAlign: 'center',
-              display: 'flex',
-            }}
-          >
-            <Box className='text'
-              sx={{
-                width:'auto',
-                height: 'auto',
-                position: 'relative',
-                textAlign: 'center',
-                display: 'flex',
-                color:'black',
-                margin: '5px',
-                fontFamily: 'Montserrat, sans-serif',
-                fontSize: {xs:'35px', md:'50px'},
-                fontWeight: '400'
-              }} >
-              <p style={{ border: 'dashed 2px black', position:'relative', padding: '10px'}} >
-                ABOUT US
-              </p>
-            </Box>
-            <Box className='sq'
-              sx={{
-                width: '10px',
-                height: '10px',
-                backgroundColor: 'black',
-                position: 'absolute',
-                left:'0',
-                top: '0'
-              }}
-            ></Box>
-            <Box className='sq'
-              sx={{
-                width: '10px',
-                height: '10px',
-                backgroundColor: 'black',
-                position: 'absolute',
-                right:'0',
-                top: '0'
-              }}></Box>
-            <Box className='sq'
-              sx={{
-                width: '10px',
-                height: '10px',
-                backgroundColor: 'black',
-                position: 'absolute',
-                left:'0',
-                bottom: '0'
-              }}></Box>
-            <Box className='sq'
-              sx={{
-                width: '10px',
-                height: '10px',
-                backgroundColor: 'black',
-                position: 'absolute',
-                right:'0',
-                bottom: '0'
-              }}></Box>
-          </Box>
-        </Box>
-      </Container>
+
+      <Box
+        sx={{
+          backgroundColor: '#10423D',
+        
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          height: {xs: '20vh',md:'55vh'},
+          alignItems: 'center', 
+          alignContent: 'center',
+          fontFamily: 'Anek Odia',
+          color:'#E2D2C6',
+          fontSize: {xs: '12px', sm: '24px', md: '34px'},
+          margin: '8vw',
+          padding: '20px'
+
+        }}
+      >
+        <h2 style={{maxWidth: '50%'}}>Build your dream residence with us.</h2>
+        <Button 
+          variant='contained'
+          size="large"
+          color="info"
+          className="fullWidth500"
+          component={Link}
+          to="/program"
+          style={{marginTop: '52px', marginBottom: '2em'}}>
+          Learn more 
+        </Button>
+        <img src="/HOUSE2.jpg"
+              style={{
+                  maxWidth:'25vw',
+                  boxShadow: '2px 2px 16px 2px rgba(0, 0, 0, 0.28)',
+                  width: 'auto',
+              }}   
+              alt="rendering"
+          />
+      </Box>
     </>
   );
 }
